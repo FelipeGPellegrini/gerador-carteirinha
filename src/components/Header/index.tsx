058f508f9b109e9a25effc929f285256d8481398
Felipe Gomes Pellegrini <lipegomespellegrini10@gmail.com>
2024-02-15 22:53:55 -0300

    header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css'
+
+
+const index = () => {
+  return (
+    <div className='containerHeader'>
+        <h1>GERADOR DE CARTEIRINHAS</h1>
+    </div>
+  )
+}
+
+export default index
